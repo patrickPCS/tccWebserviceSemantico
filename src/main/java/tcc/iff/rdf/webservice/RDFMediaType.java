c3f70d462798f1d40904a5e516d19219ad3b1660
odanilomoreira <danilokof7@gmail.com>
2018-01-31 22:49:41 -0200

    RDFMediaType

---
+++ src/main/java/tcc/iff/rdf/webservice/RDFMediaType.java
@@ -0,0 +1,46 @@
+package tcc.iff.rdf.webservice;
+
+import javax.ws.rs.core.MediaType;
+
+public class RDFMediaType {
+
+	public final static String TEXT_N3 = "text/rdf+n3";
+    public final static MediaType TEXT_N3_TYPE = new MediaType("text","rdf+n3");
+
+	public final static String APPLICATION_N3 = "application/n3";
+    public final static MediaType APPLICATION_N3_TYPE = new MediaType("application","n3");
+
+    public final static String APPLICATION_TURTLE_CURRENT = "application/x-turtle";
+    public final static MediaType APPLICATION_TURTLE_TYPE_CURRENT = new MediaType("application","x-turtle");
+    
+    public final static String APPLICATION_TURTLE_IDEAL = "application/turtle";
+    public final static MediaType APPLICATION_TURTLE_TYPE_IDEAL = new MediaType("application","turtle");
+
+    public final static String APPLICATION_RDFXML = "application/rdf+xml";
+    public final static MediaType APPLICATION_RDFXML_TYPE = new MediaType("application","rdf+xml");
+    
+    public final static String TEXT_NTRIPLES = "text/plain";
+    public final static MediaType TEXT_NTRIPLES_TYPE = new MediaType("text","plain");
+
+    public final static String APPLICATION_NTRIPLES = "application/n-triples";
+    public final static MediaType APPLICATION_NTRIPLES_TYPE = new MediaType("application","n-triples");
+
+    public final static String APPLICATION_SPARQL_RESULTS_XML = "application/sparql-result+xml";
+    public final static MediaType APPLICATION_SPARQL_RESULTS_XML_TYPE = new MediaType("application","sparql-result+xml");
+
+    public final static String APPLICATION_SPARQL_RESULTS_JSON = "application/sparql-result+json";
+    public final static MediaType APPLICATION_SPARQL_RESULTS_JSON_TYPE = new MediaType("application","sparql-result+json");
+
+    public final static String APPLICATION_SPARQL_QUERY_X = "application/x-sparql-query";
+    public final static MediaType APPLICATION_SPARQL_QUERY_X_TYPE = new MediaType("application","x-sparql-query");
+
+    public final static String APPLICATION_SPARQL_QUERY = "application/sparql-query";
+    public final static MediaType APPLICATION_SPARQL_QUERY_TYPE = new MediaType("application","sparql-query");
+
+    public final static String APPLICATION_SPARQL_UPDATE_X = "application/x-sparql-update";
+    public final static MediaType APPLICATION_SPARQL_UPDATE_X_TYPE = new MediaType("application","x-sparql-update");
+
+    public final static String APPLICATION_SPARQL_UPDATE = "application/sparql-update";
+    public final static MediaType APPLICATION_SPARQL_UPDATE_TYPE = new MediaType("application","sparql-update");
+
+}
--- src/main/java/tcc/iff/rdf/webservice/resources/CategoryResources.java
+++ src/main/java/tcc/iff/rdf/webservice/resources/CategoryResources.java
@@ -2,14 +2,19 @@ package tcc.iff.rdf.webservice.resources;
 
 import java.net.URI;
 
-import javax.ws.rs.GET;
+//import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+//import javax.ws.rs.core.MediaType;
+//import org.apache.jena.atlas.web.MediaType ;
+//import org.apache.jena.ext.com.google.common.net.MediaType;
 
+import tcc.iff.rdf.webservice.RDFMediaType;
 import tcc.iff.rdf.webservice.connection.Authentication;
 import tcc.iff.rdf.webservice.services.CategoryServices;
 
@@ -19,13 +24,18 @@ public class CategoryResources {
 	Authentication auth = new Authentication();
 	CategoryServices catServices = new CategoryServices();
 	
+	/*
 	@GET
-	public String getCategories() {
-		return "Teste Ok";
-	}
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getCategories() {
+		auth.getAuthentication();
+        return "Got categories!";
+    }
 	
+		*/
 	@POST
 	@Path("/{newCat}")
+	@Produces(RDFMediaType.APPLICATION_SPARQL_UPDATE)
 	public Response adicionarCategoria(@PathParam("newCat") String newCat, @Context UriInfo uriInfo) {
 		auth.getAuthentication();
 		catServices.addCategory(newCat);
@@ -35,7 +45,7 @@ public class CategoryResources {
 				.build();
 		
 	}
-	
+
 	
 	
 	
