37d49a017e4ca0f7f0b6f8a650c99ff64cb051b4
odanilomoreira <danilokof7@gmail.com>
2018-03-13 09:14:40 -0300

    Todos os métodos GET POST PUT DELETE de Categorias funcionando

--- src/main/java/tcc/iff/rdf/webservice/model/Category.java
+++ src/main/java/tcc/iff/rdf/webservice/model/Category.java
@@ -5,7 +5,16 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Category {
 	
+	private String id;
 	private String name;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
